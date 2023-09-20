2478ade47a12e5a8ba780334c3e377858ba07fb3
Name Surname <dmitriy91pozdeev@mail.ru>
2023-09-20 20:01:53 +0300

    Rework2

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
         int price_ticket = 15000; // цена билета
         int price_bonus = 20; // количество рублей за одну бонусную милю
-        int mili = price_ticket/price_bonus; // количество начисленных миль
+        int mili = price_ticket / price_bonus; // количество начисленных миль
 
 
         System.out.println("Цена билета: " + price_ticket + " рублей(я)");
