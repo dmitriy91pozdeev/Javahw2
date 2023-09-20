23379b7b2e7f29ee2d2ded2fe3c19bef7cab2c86
Name Surname <dmitriy91pozdeev@mail.ru>
2023-09-20 13:38:18 +0300

    Added code

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+
+        int a = 15000; // цена билета
+        int b = 20; // количество рублей за одну бонусную милю
+        int c = a/b; // количество начисленных миль
+
+
+        System.out.println("Цена билета " + a + " рублей(я)");
+        System.out.println("Начислено: " + c + " миль(я)");
+
+
+    }
+}
